ffcb7c036dad3ecd6090335efb11903e83b7312e
fagneralves <fagner.all@hotmail.com>
2021-08-21 16:26:57 -0300

    Aprimorando

--- ononlan.sh
+++ ononlan.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-ifconfig | grep "inet " | cut -d ' ' -f 10 | cut -d '.' -f 1-3 > /tmp/ip.txt
+ifconfig | grep "inet " | grep -v "127.0.0" | cut -d ' ' -f 10,13 # > /tmp/ipandnetmask.txt
 while read y
 do
-ip=$y
-for x in `seq 1 254`; do
-ping -c 1 $ip'.'$x | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" | grep -v "127.0.0" &
-done
-done < /tmp/ip.txt
-rm /tmp/ip.txt
+netmask=$(echo $y | cut -d " " -f 2 )
+ip=$(echo $y | cut -d '.' -f 1-3)
+{
+    netmask $(echo $y | cut -d " " -f 1)/$netmask
+} || {
+    echo "Necessario o comando netmask para calcular o range"
+    echo "Instale usando #apt -y install netmask"
+}
+exit 1
+if [ $netmask == "255.255.255.0" ];
+then
+    for x in `seq 1 254`; do
+    ping -c 1 $ip'.'$x | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
+if [ $netmask == "25525500" ];
+then
+    for x in `seq 1 254`; do
+    ping -c 1 $ip'.'$x | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
+if [ $netmask == "255000" ];
+then
+    for x in `seq 1 254`; do
+    ping -c 1 $ip'.'$x | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
+done < /tmp/ipandnetmask.txt
+#rm /tmp/ipandnetmask.txt
 
 echo "by SoenShem."

b3a09e7e94569a7d081cc2fc9c6e9f9a1ffd6415
fagneralves <fagner.all@hotmail.com>
2021-08-22 03:00:11 -0300

    2.0

--- ononlan.sh
+++ ononlan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ifconfig | grep "inet " | grep -v "127.0.0" | cut -d ' ' -f 10,13  # > /tmp/ipandnetmask.txt
+ifconfig | grep "inet " | grep -v "127.0.0" | cut -d ' ' -f 10,13 > /tmp/ipandnetmask.txt
+linhas=0
 while read y
 do
 netmask=$(echo $y | cut -d " " -f 2 )
@@ -24,37 +25,35 @@ ip=$(echo $y | cut -d ' ' -f 1)
 }
 if [ $second8bits -eq 0 ];
 then
-    echo $second8bits
     if [ $third8bits -eq 0 ];
     then
         if [ $quarter8bits -eq 0 ];
         then
             echo "Range vazia."
+            exit 1
         else
-        for quarter in `seq $(echo $ipstart | cut -d '.' -f 4) $(echo $maxip | cut -d '.' -f 4)`;do
-            echo $(echo $ip | cut -d "." -f 1-3)".$quarter"
-            #ping -c 1 $(echo $ip | cut -d '.' -f 1-)'.'$second | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-        done
+            for quarter in `seq 1 $(echo $maxip | cut -d '.' -f 4)`;do
+                ping -b -c 1 $(echo $ipstart | cut -d '.' -f 1-3).$quarter | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+            done
         fi
     else
         for third in `seq $(echo $ipstart | cut -d '.' -f 3) $(echo $maxip | cut -d '.' -f 3)`;do
-            for quarter in `seq $(echo $ipstart | cut -d '.' -f 4) $(echo $maxip | cut -d '.' -f 4)`;do
-                echo $(echo $ip | cut -d "." -f 1-2)".$third.$quarter"
-                #ping -c 1 $(echo $ip | cut -d '.' -f 1-)'.'$second | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+            for quarter in `seq 1 $(echo $maxip | cut -d '.' -f 4)`;do
+                ping -b -c 1 $(echo $ipstart | cut -d '.' -f 1-2).$third.$quarter | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
             done
         done
     fi
 else
     for second in `seq $(echo $ipstart | cut -d '.' -f 2) $(echo $maxip | cut -d '.' -f 2)`;do
        for third in `seq $(echo $ipstart | cut -d '.' -f 3) $(echo $maxip | cut -d '.' -f 3)`;do
-            for quarter in `seq $(echo $ipstart | cut -d '.' -f 4) $(echo $maxip | cut -d '.' -f 4)`;do
-                echo $(echo $ip | cut -d "." -f 1)".$second.$third.$quarter"
-                #ping -c 1 $(echo $ip | cut -d '.' -f 1-)'.'$second | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+            for quarter in `seq 1 $(echo $maxip | cut -d '.' -f 4)`;do
+                ping -b -c 1 $(echo $ipstart | cut -d '.' -f 1).$second.$third.$quarter | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
             done
         done
     done
 fi
 done < /tmp/ipandnetmask.txt
-#rm /tmp/ipandnetmask.txt
+rm /tmp/ipandnetmask.txt
 
 echo "by SoenShem."
+exit 1
